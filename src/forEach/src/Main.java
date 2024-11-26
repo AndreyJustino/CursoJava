2e2aeff49b06113f71e201282bf9a5ae79348650
AndreyJustino <andreyjustino2020@gmail.com>
2024-11-26 20:13:59 -0300

    feat: aula sobre for each

---
+++ src/forEach/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] vect = new String[]{"Deus", "Jesus", "Andrey", "Adriana"};
+
+        for (String value : vect){
+            System.out.printf("%s%n",value.toUpperCase());
+        }
+    }
+}
